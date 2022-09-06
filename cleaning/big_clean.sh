d1f978f7fd59ff1cb9dc63d1c5b3b2936cc8cf44
Max Quintavalle <quint218@rocinante.morris.umn.edu>
2022-09-06 16:25:23 -0500

    Make big_clean.sh take the "cleaned" file and make a file called cleaned_(input file name) with the changed content and move it into the cleaning directory.

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -2,9 +2,18 @@
 input=$1
 
 Scratch=$(mktemp --directory)
+here=$(pwd)
 
-tar xvf $input --directory $Scratch
+tar -xvf $input --directory=$Scratch
 
 grep -lr "DELETE ME!" $Scratch | xargs rm -f
 
+cd $Scratch
+
+tar -czvf "cleaned_$input" *
+
+mv "cleaned_$input" $here
+
+
 rm -rf $Scratch
+
